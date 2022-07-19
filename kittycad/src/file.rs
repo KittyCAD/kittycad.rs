24f71a7fe880e38df684423c97dbaf9e4f33439f
Jess Frazelle <github@jessfraz.com>
2022-07-18 19:28:49 -0700

    fixups
    Signed-off-by: Jess Frazelle <github@jessfraz.com>

--- kittycad/src/file.rs
+++ kittycad/src/file.rs
@@ -21,8 +21,8 @@ impl File {
              example_file_create_conversion() -> anyhow::Result<()> {\n    let client = \
              kittycad::Client::new_from_env();\n    let result: kittycad::types::FileConversion = \
              client\n        .file()\n        .create_conversion(\n            \
-             kittycad::types::FileOutputFormat::Fbx,\n            \
-             kittycad::types::FileSourceFormat::Fbx,\n            \
+             kittycad::types::FileOutputFormat::Step,\n            \
+             kittycad::types::FileSourceFormat::Stl,\n            \
              &bytes::Bytes::from(\"some-string\"),\n        )\n        .await?;\n    \
              println!(\"{:?}\", result);\n    Ok(())\n}\n```"]
     pub async fn create_conversion<'a>(
@@ -128,7 +128,14 @@ impl File {
         }
     }
 
-    #[doc = "Execute a KittyCAD program in a specific language.\n\n```rust,no_run\nasync fn example_file_create_execution() -> anyhow::Result<()> {\n    let client = kittycad::Client::new_from_env();\n    let result: kittycad::types::CodeOutput = client\n        .file()\n        .create_execution(\n            kittycad::types::CodeLanguage::Go,\n            Some(\"some-string\".to_string()),\n            &bytes::Bytes::from(\"some-string\"),\n        )\n        .await?;\n    println!(\"{:?}\", result);\n    Ok(())\n}\n```"]
+    #[doc = "Execute a KittyCAD program in a specific language.\n\n```rust,no_run\nasync fn \
+             example_file_create_execution() -> anyhow::Result<()> {\n    let client = \
+             kittycad::Client::new_from_env();\n    let result: kittycad::types::CodeOutput = \
+             client\n        .file()\n        .create_execution(\n            \
+             kittycad::types::CodeLanguage::Node,\n            \
+             Some(\"some-string\".to_string()),\n            \
+             &bytes::Bytes::from(\"some-string\"),\n        )\n        .await?;\n    \
+             println!(\"{:?}\", result);\n    Ok(())\n}\n```"]
     pub async fn create_execution<'a>(
         &'a self,
         lang: crate::types::CodeLanguage,
@@ -174,7 +181,7 @@ impl File {
              example_file_create_mass() -> anyhow::Result<()> {\n    let client = \
              kittycad::Client::new_from_env();\n    let result: kittycad::types::FileMass = \
              client\n        .file()\n        .create_mass(\n            3.14 as f64,\n            \
-             kittycad::types::FileSourceFormat::Step,\n            \
+             kittycad::types::FileSourceFormat::Dae,\n            \
              &bytes::Bytes::from(\"some-string\"),\n        )\n        .await?;\n    \
              println!(\"{:?}\", result);\n    Ok(())\n}\n```"]
     pub async fn create_mass<'a>(
@@ -216,7 +223,7 @@ impl File {
              example_file_create_volume() -> anyhow::Result<()> {\n    let client = \
              kittycad::Client::new_from_env();\n    let result: kittycad::types::FileVolume = \
              client\n        .file()\n        .create_volume(\n            \
-             kittycad::types::FileSourceFormat::Stl,\n            \
+             kittycad::types::FileSourceFormat::Dae,\n            \
              &bytes::Bytes::from(\"some-string\"),\n        )\n        .await?;\n    \
              println!(\"{:?}\", result);\n    Ok(())\n}\n```"]
     pub async fn create_volume<'a>(
