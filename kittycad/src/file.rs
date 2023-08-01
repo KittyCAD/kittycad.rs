b752dfb25b38c1e53418d6ad11a744aeffac908b
Jess Frazelle <jessfraz@users.noreply.github.com>
2023-07-31 19:32:10 -0700

    Update api spec (#214)
    * YOYO NEW API SPEC!
    
    * closes #215
    
    Signed-off-by: Jess Frazelle <github@jessfraz.com>
    
    * updates
    
    Signed-off-by: Jess Frazelle <github@jessfraz.com>
    
    ---------
    
    Signed-off-by: Jess Frazelle <github@jessfraz.com>
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>

--- kittycad/src/file.rs
+++ kittycad/src/file.rs
@@ -12,10 +12,11 @@ impl File {
         Self { client }
     }
 
-    #[doc = "Get CAD file center of mass.\n\nWe assume any file given to us has one consistent unit throughout. We also assume the file is at the proper scale.\nCurrently, this endpoint returns the cartesian co-ordinate in world space measure units.\nIn the future, we will use the units inside the file if they are given and do any conversions if necessary for the calculation. But currently, that is not supported.\nGet the center of mass of an object in a CAD file. If the file is larger than 25MB, it will be performed asynchronously.\nIf the operation is performed asynchronously, the `id` of the operation will be returned. You can use the `id` returned from the request to get status information about the async operation from the `/async/operations/{id}` endpoint.\n\n**Parameters:**\n\n- `src_format: crate::types::FileImportFormat`: The format of the file. (required)\n\n```rust,no_run\nasync fn example_file_create_center_of_mass() -> anyhow::Result<()> {\n    let client = kittycad::Client::new_from_env();\n    let result: kittycad::types::FileCenterOfMass = client\n        .file()\n        .create_center_of_mass(\n            kittycad::types::FileImportFormat::Stl,\n            &bytes::Bytes::from(\"some-string\"),\n        )\n        .await?;\n    println!(\"{:?}\", result);\n    Ok(())\n}\n```"]
+    #[doc = "Get CAD file center of mass.\n\nWe assume any file given to us has one consistent unit throughout. We also assume the file is at the proper scale.\nCurrently, this endpoint returns the cartesian co-ordinate in world space measure units.\nIn the future, we will use the units inside the file if they are given and do any conversions if necessary for the calculation. But currently, that is not supported.\nGet the center of mass of an object in a CAD file. If the file is larger than 25MB, it will be performed asynchronously.\nIf the operation is performed asynchronously, the `id` of the operation will be returned. You can use the `id` returned from the request to get status information about the async operation from the `/async/operations/{id}` endpoint.\n\n**Parameters:**\n\n- `output_unit: Option<crate::types::UnitLength>`: The output unit for the center of mass.\n- `src_format: crate::types::FileImportFormat`: The format of the file. (required)\n\n```rust,no_run\nasync fn example_file_create_center_of_mass() -> anyhow::Result<()> {\n    let client = kittycad::Client::new_from_env();\n    let result: kittycad::types::FileCenterOfMass = client\n        .file()\n        .create_center_of_mass(\n            Some(kittycad::types::UnitLength::Yd),\n            kittycad::types::FileImportFormat::Stl,\n            &bytes::Bytes::from(\"some-string\"),\n        )\n        .await?;\n    println!(\"{:?}\", result);\n    Ok(())\n}\n```"]
     #[tracing::instrument]
     pub async fn create_center_of_mass<'a>(
         &'a self,
+        output_unit: Option<crate::types::UnitLength>,
         src_format: crate::types::FileImportFormat,
         body: &bytes::Bytes,
     ) -> Result<crate::types::FileCenterOfMass, crate::types::error::Error> {
@@ -24,7 +25,11 @@ impl File {
             format!("{}/{}", self.client.base_url, "file/center-of-mass"),
         );
         req = req.bearer_auth(&self.client.token);
-        let query_params = vec![("src_format", format!("{}", src_format))];
+        let mut query_params = vec![("src_format", format!("{}", src_format))];
+        if let Some(p) = output_unit {
+            query_params.push(("output_unit", format!("{}", p)));
+        }
+
         req = req.query(&query_params);
         req = req.body(body.clone());
         let resp = req.send().await?;
@@ -105,11 +110,15 @@ impl File {
              returned. You can use the `id` returned from the request to get status information \
              about the async operation from the `/async/operations/{id}` \
              endpoint.\n\n**Parameters:**\n\n- `material_mass: f64`: The material mass. \
-             (required)\n- `src_format: crate::types::FileImportFormat`: The format of the file. \
-             (required)\n\n```rust,no_run\nasync fn example_file_create_density() -> \
+             (required)\n- `material_mass_unit: Option<crate::types::UnitMass>`: The unit of the \
+             material mass.\n- `output_unit: Option<crate::types::UnitDensity>`: The output unit \
+             for the density.\n- `src_format: crate::types::FileImportFormat`: The format of the \
+             file. (required)\n\n```rust,no_run\nasync fn example_file_create_density() -> \
              anyhow::Result<()> {\n    let client = kittycad::Client::new_from_env();\n    let \
              result: kittycad::types::FileDensity = client\n        .file()\n        \
              .create_density(\n            3.14 as f64,\n            \
+             Some(kittycad::types::UnitMass::Lb),\n            \
+             Some(kittycad::types::UnitDensity::KgM3),\n            \
              kittycad::types::FileImportFormat::Stl,\n            \
              &bytes::Bytes::from(\"some-string\"),\n        )\n        .await?;\n    \
              println!(\"{:?}\", result);\n    Ok(())\n}\n```"]
@@ -117,6 +126,8 @@ impl File {
     pub async fn create_density<'a>(
         &'a self,
         material_mass: f64,
+        material_mass_unit: Option<crate::types::UnitMass>,
+        output_unit: Option<crate::types::UnitDensity>,
         src_format: crate::types::FileImportFormat,
         body: &bytes::Bytes,
     ) -> Result<crate::types::FileDensity, crate::types::error::Error> {
@@ -125,10 +136,18 @@ impl File {
             format!("{}/{}", self.client.base_url, "file/density"),
         );
         req = req.bearer_auth(&self.client.token);
-        let query_params = vec![
+        let mut query_params = vec![
             ("material_mass", format!("{}", material_mass)),
             ("src_format", format!("{}", src_format)),
         ];
+        if let Some(p) = material_mass_unit {
+            query_params.push(("material_mass_unit", format!("{}", p)));
+        }
+
+        if let Some(p) = output_unit {
+            query_params.push(("output_unit", format!("{}", p)));
+        }
+
         req = req.query(&query_params);
         req = req.body(body.clone());
         let resp = req.send().await?;
@@ -146,11 +165,13 @@ impl File {
         }
     }
 
-    #[doc = "Get CAD file mass.\n\nWe assume any file given to us has one consistent unit throughout. We also assume the file is at the proper scale.\nCurrently, this endpoint assumes if you are giving a material density in a specific mass unit per cubic measure unit, we return a mass in mass units. The same mass units as passed in the material density.\nIn the future, we will use the units inside the file if they are given and do any conversions if necessary for the calculation. But currently, that is not supported.\nGet the mass of an object in a CAD file. If the file is larger than 25MB, it will be performed asynchronously.\nIf the operation is performed asynchronously, the `id` of the operation will be returned. You can use the `id` returned from the request to get status information about the async operation from the `/async/operations/{id}` endpoint.\n\n**Parameters:**\n\n- `material_density: f64`: The material density. (required)\n- `src_format: crate::types::FileImportFormat`: The format of the file. (required)\n\n```rust,no_run\nasync fn example_file_create_mass() -> anyhow::Result<()> {\n    let client = kittycad::Client::new_from_env();\n    let result: kittycad::types::FileMass = client\n        .file()\n        .create_mass(\n            3.14 as f64,\n            kittycad::types::FileImportFormat::Stl,\n            &bytes::Bytes::from(\"some-string\"),\n        )\n        .await?;\n    println!(\"{:?}\", result);\n    Ok(())\n}\n```"]
+    #[doc = "Get CAD file mass.\n\nWe assume any file given to us has one consistent unit throughout. We also assume the file is at the proper scale.\nCurrently, this endpoint assumes if you are giving a material density in a specific mass unit per cubic measure unit, we return a mass in mass units. The same mass units as passed in the material density.\nIn the future, we will use the units inside the file if they are given and do any conversions if necessary for the calculation. But currently, that is not supported.\nGet the mass of an object in a CAD file. If the file is larger than 25MB, it will be performed asynchronously.\nIf the operation is performed asynchronously, the `id` of the operation will be returned. You can use the `id` returned from the request to get status information about the async operation from the `/async/operations/{id}` endpoint.\n\n**Parameters:**\n\n- `material_density: f64`: The material density. (required)\n- `material_density_unit: Option<crate::types::UnitDensity>`: The unit of the material density.\n- `output_unit: Option<crate::types::UnitMass>`: The output unit for the mass.\n- `src_format: crate::types::FileImportFormat`: The format of the file. (required)\n\n```rust,no_run\nasync fn example_file_create_mass() -> anyhow::Result<()> {\n    let client = kittycad::Client::new_from_env();\n    let result: kittycad::types::FileMass = client\n        .file()\n        .create_mass(\n            3.14 as f64,\n            Some(kittycad::types::UnitDensity::KgM3),\n            Some(kittycad::types::UnitMass::Lb),\n            kittycad::types::FileImportFormat::Stl,\n            &bytes::Bytes::from(\"some-string\"),\n        )\n        .await?;\n    println!(\"{:?}\", result);\n    Ok(())\n}\n```"]
     #[tracing::instrument]
     pub async fn create_mass<'a>(
         &'a self,
         material_density: f64,
+        material_density_unit: Option<crate::types::UnitDensity>,
+        output_unit: Option<crate::types::UnitMass>,
         src_format: crate::types::FileImportFormat,
         body: &bytes::Bytes,
     ) -> Result<crate::types::FileMass, crate::types::error::Error> {
@@ -159,10 +180,18 @@ impl File {
             format!("{}/{}", self.client.base_url, "file/mass"),
         );
         req = req.bearer_auth(&self.client.token);
-        let query_params = vec![
+        let mut query_params = vec![
             ("material_density", format!("{}", material_density)),
             ("src_format", format!("{}", src_format)),
         ];
+        if let Some(p) = material_density_unit {
+            query_params.push(("material_density_unit", format!("{}", p)));
+        }
+
+        if let Some(p) = output_unit {
+            query_params.push(("output_unit", format!("{}", p)));
+        }
+
         req = req.query(&query_params);
         req = req.body(body.clone());
         let resp = req.send().await?;
@@ -180,10 +209,28 @@ impl File {
         }
     }
 
-    #[doc = "Get CAD file surface area.\n\nWe assume any file given to us has one consistent unit throughout. We also assume the file is at the proper scale.\nCurrently, this endpoint returns the square measure units.\nIn the future, we will use the units inside the file if they are given and do any conversions if necessary for the calculation. But currently, that is not supported.\nGet the surface area of an object in a CAD file. If the file is larger than 25MB, it will be performed asynchronously.\nIf the operation is performed asynchronously, the `id` of the operation will be returned. You can use the `id` returned from the request to get status information about the async operation from the `/async/operations/{id}` endpoint.\n\n**Parameters:**\n\n- `src_format: crate::types::FileImportFormat`: The format of the file. (required)\n\n```rust,no_run\nasync fn example_file_create_surface_area() -> anyhow::Result<()> {\n    let client = kittycad::Client::new_from_env();\n    let result: kittycad::types::FileSurfaceArea = client\n        .file()\n        .create_surface_area(\n            kittycad::types::FileImportFormat::Stl,\n            &bytes::Bytes::from(\"some-string\"),\n        )\n        .await?;\n    println!(\"{:?}\", result);\n    Ok(())\n}\n```"]
+    #[doc = "Get CAD file surface area.\n\nWe assume any file given to us has one consistent unit \
+             throughout. We also assume the file is at the proper scale.\nCurrently, this endpoint \
+             returns the square measure units.\nIn the future, we will use the units inside the \
+             file if they are given and do any conversions if necessary for the calculation. But \
+             currently, that is not supported.\nGet the surface area of an object in a CAD file. \
+             If the file is larger than 25MB, it will be performed asynchronously.\nIf the \
+             operation is performed asynchronously, the `id` of the operation will be returned. \
+             You can use the `id` returned from the request to get status information about the \
+             async operation from the `/async/operations/{id}` endpoint.\n\n**Parameters:**\n\n- \
+             `output_unit: Option<crate::types::UnitArea>`: The output unit for the surface \
+             area.\n- `src_format: crate::types::FileImportFormat`: The format of the file. \
+             (required)\n\n```rust,no_run\nasync fn example_file_create_surface_area() -> \
+             anyhow::Result<()> {\n    let client = kittycad::Client::new_from_env();\n    let \
+             result: kittycad::types::FileSurfaceArea = client\n        .file()\n        \
+             .create_surface_area(\n            Some(kittycad::types::UnitArea::Yd2),\n            \
+             kittycad::types::FileImportFormat::Stl,\n            \
+             &bytes::Bytes::from(\"some-string\"),\n        )\n        .await?;\n    \
+             println!(\"{:?}\", result);\n    Ok(())\n}\n```"]
     #[tracing::instrument]
     pub async fn create_surface_area<'a>(
         &'a self,
+        output_unit: Option<crate::types::UnitArea>,
         src_format: crate::types::FileImportFormat,
         body: &bytes::Bytes,
     ) -> Result<crate::types::FileSurfaceArea, crate::types::error::Error> {
@@ -192,7 +239,11 @@ impl File {
             format!("{}/{}", self.client.base_url, "file/surface-area"),
         );
         req = req.bearer_auth(&self.client.token);
-        let query_params = vec![("src_format", format!("{}", src_format))];
+        let mut query_params = vec![("src_format", format!("{}", src_format))];
+        if let Some(p) = output_unit {
+            query_params.push(("output_unit", format!("{}", p)));
+        }
+
         req = req.query(&query_params);
         req = req.body(body.clone());
         let resp = req.send().await?;
@@ -218,17 +269,20 @@ impl File {
              file is larger than 25MB, it will be performed asynchronously.\nIf the operation is \
              performed asynchronously, the `id` of the operation will be returned. You can use the \
              `id` returned from the request to get status information about the async operation \
-             from the `/async/operations/{id}` endpoint.\n\n**Parameters:**\n\n- `src_format: \
+             from the `/async/operations/{id}` endpoint.\n\n**Parameters:**\n\n- `output_unit: \
+             Option<crate::types::UnitVolume>`: The output unit for the volume.\n- `src_format: \
              crate::types::FileImportFormat`: The format of the file. \
              (required)\n\n```rust,no_run\nasync fn example_file_create_volume() -> \
              anyhow::Result<()> {\n    let client = kittycad::Client::new_from_env();\n    let \
              result: kittycad::types::FileVolume = client\n        .file()\n        \
-             .create_volume(\n            kittycad::types::FileImportFormat::Stl,\n            \
+             .create_volume(\n            Some(kittycad::types::UnitVolume::Ml),\n            \
+             kittycad::types::FileImportFormat::Stl,\n            \
              &bytes::Bytes::from(\"some-string\"),\n        )\n        .await?;\n    \
              println!(\"{:?}\", result);\n    Ok(())\n}\n```"]
     #[tracing::instrument]
     pub async fn create_volume<'a>(
         &'a self,
+        output_unit: Option<crate::types::UnitVolume>,
         src_format: crate::types::FileImportFormat,
         body: &bytes::Bytes,
     ) -> Result<crate::types::FileVolume, crate::types::error::Error> {
@@ -237,7 +291,11 @@ impl File {
             format!("{}/{}", self.client.base_url, "file/volume"),
         );
         req = req.bearer_auth(&self.client.token);
-        let query_params = vec![("src_format", format!("{}", src_format))];
+        let mut query_params = vec![("src_format", format!("{}", src_format))];
+        if let Some(p) = output_unit {
+            query_params.push(("output_unit", format!("{}", p)));
+        }
+
         req = req.query(&query_params);
         req = req.body(body.clone());
         let resp = req.send().await?;
