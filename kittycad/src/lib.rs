41337274e1a57d0ef622bd1a2416be90f5d3c4ee
Jess Frazelle <github@jessfraz.com>
2025-03-29 17:23:35 -0700

    fixups
    Signed-off-by: Jess Frazelle <github@jessfraz.com>

--- kittycad/src/lib.rs
+++ kittycad/src/lib.rs
@@ -29,7 +29,7 @@
 //!
 //! ```toml
 //! [dependencies]
-//! kittycad = "0.3.33"
+//! kittycad = "0.3.34"
 //! ```
 //!
 //! ## Basic example
--- openapitor/src/types/multipart.rs
+++ openapitor/src/types/multipart.rs
@@ -1,12 +1,14 @@
 //! Multipart form data types.
 
+use std::path::PathBuf;
+
 /// An attachement to a multipart form.
 #[derive(Debug, Clone, PartialEq, Eq, Hash)]
 pub struct Attachment {
     /// The name of the field.
     pub name: String,
-    /// The filename of the attachment.
-    pub filename: Option<String>,
+    /// The file path of the attachment.
+    pub filepath: Option<PathBuf>,
     /// The content type of the attachment.
     pub content_type: Option<String>,
     /// The data of the attachment.
@@ -18,8 +20,8 @@ impl std::convert::TryFrom<Attachment> for reqwest::multipart::Part {
 
     fn try_from(attachment: Attachment) -> Result<Self, Self::Error> {
         let mut part = reqwest::multipart::Part::bytes(attachment.data);
-        if let Some(filename) = attachment.filename {
-            part = part.file_name(filename);
+        if let Some(filepath) = attachment.filepath {
+            part = part.file_name(filepath.to_string_lossy().to_string());
         }
         if let Some(content_type) = attachment.content_type {
             part = part.mime_str(&content_type)?;
@@ -32,22 +34,12 @@ impl std::convert::TryFrom<std::path::PathBuf> for Attachment {
     type Error = std::io::Error;
 
     fn try_from(path: std::path::PathBuf) -> Result<Self, Self::Error> {
-        let filename = path
-            .file_name()
-            .ok_or_else(|| {
-                std::io::Error::new(std::io::ErrorKind::InvalidData, "invalid filename")
-            })?
-            .to_str()
-            .ok_or_else(|| {
-                std::io::Error::new(std::io::ErrorKind::InvalidData, "invalid filename")
-            })?
-            .to_string();
         // Get the mime type of the file.
         let content_type = mime_guess::from_path(&path).first_raw();
-        let data = std::fs::read(path)?;
+        let data = std::fs::read(&path)?;
         Ok(Attachment {
             name: "file".to_string(),
-            filename: Some(filename),
+            filepath: Some(path),
             content_type: content_type.map(|s| s.to_string()),
             data,
         })
