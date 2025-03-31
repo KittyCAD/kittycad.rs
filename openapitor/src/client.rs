6b0acc3c67408fb71325307c8cf79b15a7b94275
Max Ammann <max.ammann@zoo.dev>
2025-03-31 13:52:22 +0200

    fix

--- openapitor/src/client.rs
+++ openapitor/src/client.rs
@@ -330,7 +330,7 @@ impl Client {
     /// an &str (`String` or `Vec<u8>` for example). As long as the function is
     /// given a valid API key your requests will work.
     /// Also takes reqwest client builders, for customizing the client's behaviour.
-    #[tracing::instrument(skip(token)]
+    #[tracing::instrument(skip(token))]
     #[cfg(not(target_arch = "wasm32"))]
     pub fn new_from_reqwest<T>(
         token: T,
@@ -393,7 +393,7 @@ impl Client {
     /// an &str (`String` or `Vec<u8>` for example). As long as the function is
     /// given a valid API key your requests will work.
     /// Also takes reqwest client builders, for customizing the client's behaviour.
-    #[tracing::instrument(skip(token)]
+    #[tracing::instrument(skip(token))]
     #[cfg(target_arch = "wasm32")]
     pub fn new_from_reqwest<T>(
         token: T,
@@ -445,7 +445,7 @@ impl Client {
     /// Create a new Client struct. It takes a type that can convert into
     /// an &str (`String` or `Vec<u8>` for example). As long as the function is
     /// given a valid API key your requests will work.
-    #[tracing::instrument(skip(token)]
+    #[tracing::instrument(skip(token))]
     pub fn new<T>(token: T) -> Self
     where
         T: ToString + std::fmt::Debug,
